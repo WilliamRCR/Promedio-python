cf503b8ce0072a571d7a9ddf97cca17f0e2cee87
WilliamRCR <williamcastanazaromero2@gmail.com>
2021-08-14 14:54:39 -0600

    Subida

---
+++ Promedio.py
@@ -0,0 +1,6 @@
+print("Promedio de 3 numeros")
+n1 = float(input("Ingresa el primer numero"))
+n2 = float(input("Ingresa el primer numero"))
+n3 = float(input("Ingresa el primer numero"))
+promedio = (n1+n2+n3)/3
+print ("El promedio es de ", promedio)
